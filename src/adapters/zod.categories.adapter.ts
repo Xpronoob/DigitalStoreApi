50fd03cccab3e850b87156a126e6fab333a6ff12
HenryM8 <henryrm8@gmail.com>
2024-10-18 12:56:29 -0600

    Integration of CategoryEntity to zod adapter

--- src/adapters/zod.categories.adapter.ts
+++ src/adapters/zod.categories.adapter.ts
@@ -1,12 +1,13 @@
 import { z } from "zod";
 import { CustomError } from "../errors/custom.error";
+import {  CategoryEntity } from "../entities/category.entity";
 
 export class ZodCategoriesAdapter {
 
-  static validateCategory = (category: { name: string }) => {
+  static validateCategory = (category: CategoryEntity) => {
 
     const Category = z.object({
-      name: z.string({
+      category_name: z.string({
         required_error: "El nombre de la categoría es requerido"
       }).min(3, "El nombre debe tener al menos 3 caracteres").max(50, "El nombre no puede tener más de 50 caracteres"),
     });
