dffef1621b30e7341e4a37c3e1dad77cf175d91e
HenryM8 <henryrm8@gmail.com>
2024-10-19 19:50:15 -0600

    Repository for product details

---
+++ src/repositories/products-details.repository.ts
@@ -0,0 +1,30 @@
+import { ProductDetailsDatasource } from "../datasources/products-details.datasource";
+import { ProductDetailEntity, ProductDetailEntityOptional } from '../entities/products-details';
+
+export class ProductDetailsRepository {
+  constructor(private readonly productDetailsDatasource: ProductDetailsDatasource) {}
+
+  async create(productDetailData: ProductDetailEntity) {
+    return await this.productDetailsDatasource.create(productDetailData);
+  }
+
+  async update(productDetailId: number, productDetailData: ProductDetailEntityOptional) {
+    return await this.productDetailsDatasource.update(productDetailId, productDetailData);
+  }
+
+  async delete(productDetailId: number) {
+    return await this.productDetailsDatasource.delete(productDetailId);
+  }
+
+  async getAll() {
+    return await this.productDetailsDatasource.getAll();
+  }
+
+  async getById(productDetailId: number) {
+    return await this.productDetailsDatasource.getById(productDetailId);
+  }
+
+  async toggleStatus(productDetailId: number, active: boolean) {
+    return await this.productDetailsDatasource.toggleStatus(productDetailId, active);
+  }
+}
