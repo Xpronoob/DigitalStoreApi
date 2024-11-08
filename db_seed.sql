7544ff1fe6d0393bd71527f8e30a78c5a17f6524
Xpronoob <kevin_03_puris@hotmail.com>
2024-11-07 23:51:09 -0600

    Database seeder updated

--- db_seed.sql
+++ db_seed.sql
@@ -1,10 +1,13 @@
 INSERT INTO roles (role_name) VALUES ('admin');
+INSERT INTO roles (role_name) VALUES ('admin_users');
 
 INSERT INTO users (email, password, first_name, last_name, phone_number, img)
 VALUES ('admin@admin.com', '$2b$10$O/IWDT0xR5W8kNGqYCwgLe38EsZ/06D60eVy5VriM5Dru26cuRAga', 'Admin', 'User', '1234567890', 'admin_image.jpg');
 
 INSERT INTO users_roles (user_id, role_id)
 VALUES (1, 1);
+INSERT INTO users_roles (user_id, role_id) 
+VALUES (1, 2);
 
 INSERT INTO categories (category_name, img, active)
 VALUES ('Electronics', 'electronics.jpg', TRUE),
